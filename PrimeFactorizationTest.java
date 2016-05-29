4fea10b4b3cd0a023c76765db01294fb7928a9cc
Drew Keller <drew@eightycolumns.com>
2016-05-28 20:43:27 -0400

    Test prime factorization of 2

--- PrimeFactorizationTest.java
+++ PrimeFactorizationTest.java
@@ -9,4 +9,11 @@ public class PrimeFactorizationTest {
     int[] actual = PrimeFactorization.ofInteger(30);
     assertArrayEquals(expected, actual);
   }
+
+  @Test
+  public void testPrimeFactorizationOf2() {
+    int[] expected = {2};
+    int[] actual = PrimeFactorization.ofInteger(2);
+    assertArrayEquals(expected, actual);
+  }
 }
