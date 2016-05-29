9b1df9bb354aab960978b92df7d66a084caa3cc0
Drew Keller <drew@eightycolumns.com>
2016-05-28 20:47:03 -0400

    Re-order tests

--- PrimeFactorizationTest.java
+++ PrimeFactorizationTest.java
@@ -4,13 +4,6 @@ import static org.junit.Assert.assertArrayEquals;
 
 public class PrimeFactorizationTest {
   @Test
-  public void testPrimeFactorizationOf30() {
-    int[] expected = {2, 3, 5};
-    int[] actual = PrimeFactorization.ofInteger(30);
-    assertArrayEquals(expected, actual);
-  }
-
-  @Test
   public void testPrimeFactorizationOf2() {
     int[] expected = {2};
     int[] actual = PrimeFactorization.ofInteger(2);
@@ -28,6 +21,13 @@ public class PrimeFactorizationTest {
   public void testPrimeFactorizationOf5() {
     int[] expected = {5};
     int[] actual = PrimeFactorization.ofInteger(5);
+    assertArrayEquals(expected, actual);
+  }
+
+  @Test
+  public void testPrimeFactorizationOf30() {
+    int[] expected = {2, 3, 5};
+    int[] actual = PrimeFactorization.ofInteger(30);
     assertArrayEquals(expected, actual);
   }
 }
