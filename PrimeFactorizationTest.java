7f4fdbdb415f9855008786dcf175296c48f75d24
Drew Keller <drew@eightycolumns.com>
2016-05-30 19:41:18 -0400

    Tweak tests

--- PrimeFactorizationTest.java
+++ PrimeFactorizationTest.java
@@ -16,64 +16,73 @@ public class PrimeFactorizationTest {
 
   @Test
   public void thePrimeFactorizationOf1IsAnEmptySet() {
-    int[] expected = {};
-    int[] actual = PrimeFactorization.ofInteger(1);
-    assertArrayEquals(expected, actual);
+    assertArrayEquals(
+      new int[] {},
+      PrimeFactorization.ofInteger(1)
+    );
   }
 
   @Test
   public void thePrimeFactorizationOf2Is2() {
-    int[] expected = {2};
-    int[] actual = PrimeFactorization.ofInteger(2);
-    assertArrayEquals(expected, actual);
+    assertArrayEquals(
+      new int[] {2},
+      PrimeFactorization.ofInteger(2)
+    );
   }
 
   @Test
   public void thePrimeFactorizationOf3Is3() {
-    int[] expected = {3};
-    int[] actual = PrimeFactorization.ofInteger(3);
-    assertArrayEquals(expected, actual);
+    assertArrayEquals(
+      new int[] {3},
+      PrimeFactorization.ofInteger(3)
+    );
   }
 
   @Test
   public void thePrimeFactorizationOf5Is5() {
-    int[] expected = {5};
-    int[] actual = PrimeFactorization.ofInteger(5);
-    assertArrayEquals(expected, actual);
+    assertArrayEquals(
+      new int[] {5},
+      PrimeFactorization.ofInteger(5)
+    );
   }
 
   @Test
   public void thePrimeFactorizationOf30Is2And3And5() {
-    int[] expected = {2, 3, 5};
-    int[] actual = PrimeFactorization.ofInteger(30);
-    assertArrayEquals(expected, actual);
+    assertArrayEquals(
+      new int[] {2, 3, 5},
+      PrimeFactorization.ofInteger(30)
+    );
   }
 
   @Test
   public void thePrimeFactorizationOf60Is2And2And3And5() {
-    int[] expected = {2, 2, 3, 5};
-    int[] actual = PrimeFactorization.ofInteger(60);
-    assertArrayEquals(expected, actual);
+    assertArrayEquals(
+      new int[] {2, 2, 3, 5},
+      PrimeFactorization.ofInteger(60)
+    );
   }
 
   @Test
   public void thePrimeFactorizationOf90Is2And3And3And5() {
-    int[] expected = {2, 3, 3, 5};
-    int[] actual = PrimeFactorization.ofInteger(90);
-    assertArrayEquals(expected, actual);
+    assertArrayEquals(
+      new int[] {2, 3, 3, 5},
+      PrimeFactorization.ofInteger(90)
+    );
   }
 
   @Test
   public void thePrimeFactorizationOf150Is2And3And5And5() {
-    int[] expected = {2, 3, 5, 5};
-    int[] actual = PrimeFactorization.ofInteger(150);
-    assertArrayEquals(expected, actual);
+    assertArrayEquals(
+      new int[] {2, 3, 5, 5},
+      PrimeFactorization.ofInteger(150)
+    );
   }
 
   @Test
   public void thePrimeFactorizationOf3030Is2And3And5And101() {
-    int[] expected = {2, 3, 5, 101};
-    int[] actual = PrimeFactorization.ofInteger(3030);
-    assertArrayEquals(expected, actual);
+    assertArrayEquals(
+      new int[] {2, 3, 5, 101},
+      PrimeFactorization.ofInteger(3030)
+    );
   }
 }
